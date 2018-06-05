707cc2db917e21a927929ddf7ce209eb4ba89dc9
Tamir <tamirsnarf@gmail.com>
2018-06-05 09:49:30 -0500

    removed .idea

---
+++ src/lab14/prob5.java
@@ -0,0 +1,20 @@
+package lab14;
+
+import java.util.List;
+import java.util.Set;
+
+public class prob5 {
+
+    private List<Integer> firstSumK(int[] list){
+         Set<Integer>[][] arr = new Set<Integer>[list.length][11];
+         for(int i=0; i< list.length; i++){
+             for(int j=0; j<10; j++){
+
+             }
+         }
+    }
+
+    public static void main(String[] args){
+
+    }
+}
